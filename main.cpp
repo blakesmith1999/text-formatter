205a1ee5f607def3f42c2bfcd9f649b895fb964b
Blake <gn4514@wayne.edu>
2021-07-06 10:36:22 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,29 +3,30 @@
 #include <string>
 
 
-int get_lines(std::ifstream&);
+int get_lines(std::fstream&);
+void format_lines(std::fstream&);
 
-int main(){
+int main() {
 
-	std::ifstream file;
+	std::fstream file;
 
 	file.open("text.txt");
 
 	int number_of_lines = get_lines(file);
 
 	for (int i = 0; i < number_of_lines; i++) {
-
+		format_lines(file);
 	}
 
 }
 
-int get_lines(std::ifstream &file) {
-	std::string line ("");
+int get_lines(std::fstream& file) {
+	std::string line("");
 	int num_lines;
 
 	do {
 		getline(file, line);
-		
+
 		if (line.at(0)) {
 			num_lines++;
 		}
@@ -33,4 +34,14 @@ int get_lines(std::ifstream &file) {
 	} while (!file.eof());
 
 	return num_lines;
+}
+
+void format_lines(std::fstream& file) {
+	std::string line("");
+	if (line != "") {
+		for (std::string::iterator it = line.begin(); *it < line[5]; it++) {
+			line.erase(*it);
+		}
+		file << line;
+	}
 }
